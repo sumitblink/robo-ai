6f5feb054ac41f5036e4675a4e51700cc81061b8
sumitblink <sumit@blinkdigital.in>
2025-04-23 13:38:23 +0530

    Testing render

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🤖 Robo Avatar Generator</h1>
+      <p>Welcome! Upload your photo and generate a futuristic avatar.</p>
+    </div>
+  );
+}
